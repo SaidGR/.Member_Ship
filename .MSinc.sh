c0ebb580b5f2bb23c6fd326a86886b5eb2c5d9e6
Said <47763265+SaidGR@users.noreply.github.com>
2019-11-18 22:01:49 -0600

    Create .MSinc.sh

---
+++ .MSinc.sh
@@ -0,0 +1,8 @@
+cd /data/data/com.termux/files/usr/etc
+mv motd motd2
+mv bash.bashrc bash.bashrc2
+cd
+cd .Member_Ship
+mv .int.sh /data/data/com.termux/files/usr/etc
+cd /data/data/com.termux/files/usr/etc
+mv .int.sh bash.bashrc
