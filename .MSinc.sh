72f588b384e471d78ead4742953137896361ebcd
Said <47763265+SaidGR@users.noreply.github.com>
2019-11-18 23:35:18 -0600

    Update .MSinc.sh

--- .MSinc.sh
+++ .MSinc.sh
@@ -1,8 +1,10 @@
+cd
 cd /data/data/com.termux/files/usr/etc
 mv motd motd2
 mv bash.bashrc bash.bashrc2
 cd
 cd .Member_Ship
+chmod 777 .int.sh
 mv .int.sh /data/data/com.termux/files/usr/etc
 cd /data/data/com.termux/files/usr/etc
 mv .int.sh bash.bashrc
