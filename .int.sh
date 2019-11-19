6c7e9ce8a174832921b57955ddc2646fcb23994c
Said <47763265+SaidGR@users.noreply.github.com>
2019-11-19 00:21:11 -0600

    Update .int.sh

--- .int.sh
+++ .int.sh
@@ -3,7 +3,8 @@ if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]; then
                 /data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
         }
 fi
-
+cd
+cd .Member_Ship
 chmod 777 .run.sh
 sh .run.sh
 
